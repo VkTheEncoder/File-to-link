14f7037d502cd54a76958c947fdd6fe914aa7572
UHD Official <uhdnetwork@gmail.com>
2025-09-20 22:33:06 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -1,3 +1,4 @@
+# 🗿  Visit & Support us - @UHD_Official
 # ⚡️ Do Not Remove Credit - Made by @UHD_Bots
 # 💬 For Any Help Join Support Group: @UHDBots_Support
 # 🚫 Removing or Modifying these Lines will Cause the bot to Stop Working.
