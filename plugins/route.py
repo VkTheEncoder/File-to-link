3b3f9a538ef78f1082f0d778c3b54446e98aee32
UHD Official <uhdnetwork@gmail.com>
2025-09-20 22:20:38 +0530

    Update route.py

--- plugins/route.py
+++ plugins/route.py
@@ -1,3 +1,4 @@
+# 🗿  Visit & Support us - @UHD_Official
 # ⚡️ Do Not Remove Credit - Made by @UHD_Bots
 # 💬 For Any Help Join Support Group: @UHDBots_Support
 # 🚫 Removing or Modifying these Lines will Cause the bot to Stop Working.
@@ -122,5 +123,6 @@ async def _stream_file(request: web.Request, file_id: int, secure_hash: str):
             "Accept-Ranges": "bytes"
         }
     )
+
 
 
